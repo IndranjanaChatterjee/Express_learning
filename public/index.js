e680830687e2c872ed8fceb610d8c85cc052f049
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-05-14 10:40:23 +0530

    Static

--- public/index.js
+++ public/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const path = require("path");
 const app=express();
+
+const source=path.join(__dirname,"../files");
+console.log(source);
+app.use(express.static(source));
 app.get("/",(req,res)=>
 {
     res.send("<h1> Hello in home page </h1>");
